004f834c0e7be5d0c1216b786532e10a909291bd
Marko Heijnen <info@markoheijnen.com>
2012-11-13 21:28:43 +0100

    Set time for DateTime object otherwise it will get the current time
    Also ignore .DS_Store files

--- src/Kmt/parser/banking/mt940/engine.php
+++ src/Kmt/parser/banking/mt940/engine.php
@@ -341,7 +341,7 @@ class Engine_mt940_banking_parser {
 	 * @return string
 	 */
 	function _sanitizeAccountName($string) {
-		return preg_replace('/[\s]+/', PHP_EOL, trim($string));
+		return trim( $string );
 	}
 
 	/**
@@ -351,6 +351,8 @@ class Engine_mt940_banking_parser {
 	 */
 	function _sanitizeTimestamp($string, $inFormat = 'ymd') {
 		$date = DateTime::createFromFormat($inFormat, $string);
+		$date->setTime(0, 0, 0);
+
 		if ($date !== false) {
 			return $date->format('U');
 		}
